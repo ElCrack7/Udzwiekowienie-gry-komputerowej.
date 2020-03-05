ecb5c1b146a05d2971714e7b27318812476677ce
Grzegorz Pietras <pietri77@gmail.com>
2020-03-05 13:36:02 +0100

    first commit

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class PauseManager : MonoBehaviour
+{
+
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpasued;
+
+    Canvas canvas;
+    
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+
+    }
+
+  
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.enabled = !canvas.enabled;
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        Lowpass();
+    }
+
+    void Lowpass()
+    {
+        if (Time.timeScale == 0)
+        {
+            paused.TransitionTo(.01f);
+        }
+        else
+        {
+            unpasued.TransitionTo(.01f);
+        }
+
+    }
+   
+}
+
